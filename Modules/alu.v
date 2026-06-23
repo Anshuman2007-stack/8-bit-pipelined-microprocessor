f982bfc03b9affc0e917b86b2e3e4a985b5853bb
Nilesh Kalra <nilesh2211kalra@gmail.com>
2026-06-24 01:30:34 +0530

    Update alu.v

--- Modules/alu.v
+++ Modules/alu.v
@@ -18,6 +18,7 @@ parameter XOR               = 4'b1111;
 parameter Right_Shift       = 4'b0001;
 parameter Left_Shift        = 4'b0010;
 parameter SLT               = 4'b0101;
+parameter LSR               = 4'b0011;
 
 always @(*) begin
     Out = 8'b0;
@@ -41,10 +42,12 @@ always @(*) begin
         XOR: Out = SrcA ^ SrcB;
         Right_Shift: Out = $signed(SrcA) >>> SrcB;
         Left_Shift: Out = SrcA << SrcB;
+        LSR: Out = SrcA >> SrcB;
         SLT:begin Out = (SrcA + (~SrcB + 8'b1));
         Overflow = (SrcA[7] != SrcB[7]) && (Out[7] != SrcA[7]) ? 1'b1 : 1'b0;
         Out = {7'b0, (Out[7] ^ Overflow)};//XOR with overflow to preserve logic for opposite sign comparisons
         end
+        
         default: Out = 8'b0;
     endcase
 
