62e705685baea0ada1c04e435960f92f3793c8cb
Nilesh Kalra <nilesh2211kalra@gmail.com>
2026-06-24 02:56:00 +0530

    Update alu.v

--- Modules/alu.v
+++ Modules/alu.v
@@ -19,6 +19,9 @@ parameter Right_Shift       = 4'b0001;
 parameter Left_Shift        = 4'b0010;
 parameter SLT               = 4'b0101;
 parameter LSR               = 4'b0011;
+parameter ROR               = 4'b0100;
+
+reg [2:0] shift_amt;
 
 always @(*) begin
     Out = 8'b0;
@@ -47,7 +50,14 @@ always @(*) begin
         Overflow = (SrcA[7] != SrcB[7]) && (Out[7] != SrcA[7]) ? 1'b1 : 1'b0;
         Out = {7'b0, (Out[7] ^ Overflow)};//XOR with overflow to preserve logic for opposite sign comparisons
         end
-        
+        ROR: begin
+       shift_amt = SrcB % 8;
+    
+        if (shift_amt == 0)
+        Out = SrcA;
+        else
+        Out = ((SrcA >> shift_amt) | (SrcA << (8 - shift_amt)));
+        end
         default: Out = 8'b0;
     endcase
 
