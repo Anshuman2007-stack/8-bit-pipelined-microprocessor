ccc87b681bc83aa017ac3fac8d6c84910a7943c2
Anshuman2007-stack <nemaanshuman007@gmail.com>
2026-06-24 13:40:18 +0530

    Update single_cycle.v

--- Single_Cycle/single_cycle.v
+++ Single_Cycle/single_cycle.v
@@ -183,6 +183,11 @@ parameter XOR               = 4'b1111;
 parameter Right_Shift       = 4'b0001;
 parameter Left_Shift        = 4'b0010;
 parameter SLT               = 4'b0101;
+parameter LSR               = 4'b0011;
+parameter ROR               = 4'b0100;
+parameter ROL               = 4'b0110;
+
+reg [2:0] shift_amt;
 
 always @(*) begin
     Out = 8'b0;
@@ -206,10 +211,28 @@ always @(*) begin
         XOR: Out = SrcA ^ SrcB;
         Right_Shift: Out = $signed(SrcA) >>> SrcB;
         Left_Shift: Out = SrcA << SrcB;
+        LSR: Out = SrcA >> SrcB;
         SLT:begin Out = (SrcA + (~SrcB + 8'b1));
         Overflow = (SrcA[7] != SrcB[7]) && (Out[7] != SrcA[7]) ? 1'b1 : 1'b0;
         Out = {7'b0, (Out[7] ^ Overflow)};//XOR with overflow to preserve logic for opposite sign comparisons
         end
+        ROR: begin
+       shift_amt = SrcB % 8;
+    
+        if (shift_amt == 0)
+        Out = SrcA;
+        else
+        Out = ((SrcA >> shift_amt) | (SrcA << (8 - shift_amt)));
+        end
+        ROL: begin
+       shift_amt = SrcB % 8;
+    
+        if (shift_amt == 0)
+        Out = SrcA;
+        else
+         Out = ((SrcA << shift_amt) | (SrcA >> (8 - shift_amt)));
+        end
+      
         default: Out = 8'b0;
     endcase
 
