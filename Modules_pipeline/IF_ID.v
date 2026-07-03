32c298a70eedc56a48d8334cd75a801f7df4848d
Nilesh Kalra <nilesh2211kalra@gmail.com>
2026-07-03 13:28:37 +0530

    Implement IF_ID module with control signals

--- Modules_pipeline/IF_ID.v
+++ Modules_pipeline/IF_ID.v
@@ -1,1 +1,36 @@
-
+`timescale 1ns / 1ps
+module IF_ID(
+input clk,
+input reset,
+input stall,
+input flush,
+input [7:0] pc_in,
+input [23:0] instruction_in,
+output reg [7:0] pc_out,
+output reg [23:0] instruction_out
+  );
+  
+ always@(posedge clk or posedge reset) begin
+ if(reset==1) begin
+ pc_out <= 8'b0;
+ instruction_out<=24'b0;
+ end
+ 
+ else if(stall==1) begin
+ pc_out<=pc_out;
+ instruction_out<=instruction_out;
+ end
+ 
+ else if(flush==1) begin
+ pc_out <= 8'b0;
+ instruction_out<=24'b0;
+ end
+ 
+ else begin
+ pc_out<=pc_in;
+ instruction_out<=instruction_in;
+ end 
+end
+ 
+  
+endmodule
